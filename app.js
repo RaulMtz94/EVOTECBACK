52b76648ffa04e12fe756817edf37dc9cd9429b4
unknown <martinezvalenzuelaraul@gmail.com>
2018-10-08 10:03:15 -0600

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //Requires importacion de librerias
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 //---------------------------------
 
 
@@ -8,20 +10,28 @@ var mongoose = require('mongoose');
 var app = express();
 
 
+//Body parser
+app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.json())
+
+//importe de rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
+
 //CONEXION A LA BASE DE DATOS
 mongoose.connection.openUri('mongodb://raul:raulmtz1@ds111993.mlab.com:11993/evotecbd' , (err , res)=>{
     if(err) throw err;
-    console.log('Base de datos MongoDB : \x1b[32m%s\x1b[0m' , 'Online');
-   
+    console.log('Base de datos MongoDB : \x1b[32m%s\x1b[0m' , 'Online');   
 });
 
 //Rutas
-app.get('/', (req , res , next) =>{
-    res.status(200).json({
-        ok : true ,
-        mensaje : 'Peticion Realizada Correctamente'
-    });
-});
+app.use('/usuario' , usuarioRoutes);
+app.use('/login' , loginRoutes);
+app.use('/' , appRoutes);
+
 
 //Escuchar peticiones
 
