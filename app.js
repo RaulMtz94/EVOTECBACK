d8b88fc2ce90b2e13b79d5e63da7fa6c050fcc0a
unknown <martinezvalenzuelaraul@gmail.com>
2018-12-04 22:20:47 -0700

    nada

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ var rutinasRoutes = require('./routes/rutinas');
 var pagosRoutes = require('./routes/pagos');
 var instructoresRoutes = require('./routes/instructores');
 var historicoseguimientoRoutes = require('./routes/historicoseguimiento');
-var cajeroRoutes = require('./routes/cajero')
 
 //CONEXION A LA BASE DE DATOS
 mongoose.connection.openUri('mongodb://raul:raulmtz1@ds111993.mlab.com:11993/evotecbd' , (err , res)=>{
@@ -66,7 +65,6 @@ app.use('/rutinas' , rutinasRoutes);
 app.use('/pagos' , pagosRoutes);
 app.use('/instructores' , instructoresRoutes);
 app.use('/historico' , historicoseguimientoRoutes);
-app.use('/cajero' , cajeroRoutes);
 
 
 
